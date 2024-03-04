7482d8ee899bca06b025a27205a4fb4f3e33e299
trygvebechsgaard <trygvemb@gmail.com>
2024-03-04 11:09:06 +0545

    database migration

--- mvc/Data/ApplicationDbContext.cs
+++ mvc/Data/ApplicationDbContext.cs
@@ -17,13 +17,48 @@ namespace mvc.Data
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Artist>()
-            .HasMany(a => a.Albums)
-            .WithOne(a => a.Artist)
-            .HasForeignKey(a => a.AlbumId);
+            // modelBuilder.Entity<Artist>()
+            // .HasMany(a => a.Albums)
+            // .WithOne(a => a.Artist)
+            // .HasForeignKey(a => a.AlbumId);
+
+            // modelBuilder.Entity<Album>()
+            // .HasMany(a => a.Songs)
+            // .WithOne(s => s.Album)
+            // .HasForeignKey(a => a.SongId);
+
+            // modelBuilder.Entity<Genre>()
+            // .HasMany(g => g.Albums)
+            // .WithOne(g => g.Genre)
+            // .HasForeignKey(a => a.AlbumId);
+
+            modelBuilder.Entity<Song>()
+            .HasMany(s => s.Playlists)
+            .WithMany(p => p.Songs)
+            .UsingEntity<Dictionary<string, object>>
+            (
+                "SongPlaylist",
+                j => j.HasOne<Playlist>()
+                .WithMany()
+                .HasForeignKey("PlaylistId")
+                .OnDelete(DeleteBehavior.Restrict),
+                j => j
+                .HasOne<Song>()
+                .WithMany()
+                .HasForeignKey("SongId")
+                .OnDelete(DeleteBehavior.Restrict)
+            );
+
+            modelBuilder.Entity<Song>()
+            .HasOne(s => s.Album)
+            .WithMany(a => a.Songs)
+            .HasForeignKey(s => s.AlbumId)
+            .OnDelete(DeleteBehavior.NoAction);
 
         }
     }
---
+++ mvc/Migrations/20240304051953_InitialCreate.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    ArtistId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.ArtistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.GenreId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Playlists",
+                columns: table => new
+                {
+                    PlaylistId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playlists", x => x.PlaylistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    AlbumId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    AlbumArtUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ArtistId = table.Column<int>(type: "int", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.AlbumId);
+                    table.ForeignKey(
+                        name: "FK_Albums_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Albums_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "GenreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    SongId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SongUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AlbumId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.SongId);
+                    table.ForeignKey(
+                        name: "FK_Songs_Albums_AlbumId",
+                        column: x => x.AlbumId,
+                        principalTable: "Albums",
+                        principalColumn: "AlbumId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SongPlaylist",
+                columns: table => new
+                {
+                    PlaylistId = table.Column<int>(type: "int", nullable: false),
+                    SongId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SongPlaylist", x => new { x.PlaylistId, x.SongId });
+                    table.ForeignKey(
+                        name: "FK_SongPlaylist_Playlists_PlaylistId",
+                        column: x => x.PlaylistId,
+                        principalTable: "Playlists",
+                        principalColumn: "PlaylistId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SongPlaylist_Songs_SongId",
+                        column: x => x.SongId,
+                        principalTable: "Songs",
+                        principalColumn: "SongId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Albums_ArtistId",
+                table: "Albums",
+                column: "ArtistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Albums_GenreId",
+                table: "Albums",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SongPlaylist_SongId",
+                table: "SongPlaylist",
+                column: "SongId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_AlbumId",
+                table: "Songs",
+                column: "AlbumId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SongPlaylist");
+
+            migrationBuilder.DropTable(
+                name: "Playlists");
+
+            migrationBuilder.DropTable(
+                name: "Songs");
+
+            migrationBuilder.DropTable(
+                name: "Albums");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
