cd0223fb1a0eace7d509ae4d835b5b65b86dc164
trygvebechsgaard <trygvemb@gmail.com>
2024-03-06 16:28:34 +0545

    crud for artist

--- mvc/Controllers/ArtistController.cs
+++ mvc/Controllers/ArtistController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using mvc.Areas.Identity.Data;
 using mvc.Interfaces;
+using mvc.Models;
 
 namespace mvc.Controllers
 {
@@ -34,6 +35,75 @@ namespace mvc.Controllers
             return View(artist);
         }
 
-        
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Artist artist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(artist);
+            }
+            await _artistRepo.AddAsync(artist);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var artist = await _artistRepo.GetByIdAsync(id);
+
+            if (artist == null) return View("Error");
+
+            return View(artist);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Artist artist, int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(artist);
+            }
+
+            artist.ArtistId = id;
+
+            _artistRepo.Update(artist);
+
+            return RedirectToAction("Detail", new { id = artist.ArtistId });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var artist = await _artistRepo.GetByIdAsync(id);
+
+            if (artist == null)
+            {
+                return View("Error");
+            }
+
+            return View(artist);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var artist = await _artistRepo.GetByIdAsync(id);
+
+            if (artist == null)
+            {
+                return View("Error");
+            }
+
+            await _artistRepo.RemoveAsync(artist);
+            return RedirectToAction("Index");
+        }
+
     }
 }
